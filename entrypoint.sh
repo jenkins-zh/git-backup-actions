9cfcd5cb7919d31c00851ef33e0ffcc161474a00
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2022-05-31 09:45:38 +0800

    try to fix git fetch errors
    fatal: unsafe repository ('/github/workspace' is owned by someone else)

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,8 @@ echo "${GIT_DEPLOY_KEY}" > /root/.ssh/id_rsa && \
 chmod 400 /root/.ssh/id_rsa
 
 echo '=================== Sync with mirror git ==================='
+git config --global --add safe.directory /github/workspace
+
 git version
 git fetch --unshallow origin
 git checkout master
@@ -20,4 +22,3 @@ git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 echo 'ready to push'
 git push backup master -f
 git remote remove backup
-
